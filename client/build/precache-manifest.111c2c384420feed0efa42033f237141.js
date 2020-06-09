3b9548c5aab4e47961af75a822ded916e49076c0
lokisixonenine <jaycee.bagtas@gmail.com>
2020-06-08 20:48:10 -0700

    changed .gitignore

---
+++ client/build/precache-manifest.111c2c384420feed0efa42033f237141.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c910b2b81fbfc68684c5295fa3b3f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6204a28d40805af0038d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "04dae7ced90a8a6ddb8b",
+    "url": "/static/css/main.bbaff036.chunk.css"
+  },
+  {
+    "revision": "6204a28d40805af0038d",
+    "url": "/static/js/2.8a127632.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8a127632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dae7ced90a8a6ddb8b",
+    "url": "/static/js/main.c4134bea.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
