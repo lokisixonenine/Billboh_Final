249824a769714b85938ec45d2f811769f7dcdfa8
Greg Schuman <gcschuman@hotmail.com>
2020-06-09 20:46:33 -0700

    Updated button names and search icon

--- client/src/components/cityHistory.js
+++ client/src/components/cityHistory.js
@@ -1,75 +1,74 @@
-import React, {useState} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import { Header, Layout, Drawer, Navigation, Content } from 'react-mdl';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import { Header, Layout, Drawer, Navigation, Content } from "react-mdl";
 
 function App() {
-  const [user,setUser] = useState(null)
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-      <div style={{height: '450px', position: 'relative'}}>
-    <Layout style={{background: 'url(https://www.hobbitontours.com/media/1174/hobbiton-movie-set-30.jpg) bottom / cover'}}>
-        <Header transparent title="Billboh" style={{color: 'white'}}>
+      <div style={{ height: "450px", position: "relative" }}>
+        <Layout
+          style={{
+            background:
+              "url(https://www.hobbitontours.com/media/1174/hobbiton-movie-set-30.jpg) bottom / cover"
+          }}
+        >
+          <Header transparent title="Billboh" style={{ color: "white" }}>
             <Navigation>
-                <a href="billboh/client/src/components/cityHistory.js">City History</a>
-                <a href="billboh/client/src/components/NewsCard.js">News</a>
-                {/* <a href="#">Link</a> */}
+              <a href="billboh/client/src/components/cityHistory.js">
+                City History
+              </a>
+              <a href="billboh/client/src/components/NewsCard.js">News</a>
+              {/* <a href="#">Link</a> */}
             </Navigation>
-        </Header>
-        <Drawer>
+          </Header>
+          <Drawer>
             <Navigation>
-                <a href="billboh/client/src/components/cityHistory.js">City History</a>
-                <a href="billboh/client/src/components/NewsCard.js">News</a>
-                {/* <a href="#">Link</a> */}
+              <a href="billboh/client/src/components/cityHistory.js">
+                City History
+              </a>
+              <a href="billboh/client/src/components/NewsCard.js">News</a>
+              {/* <a href="#">Link</a> */}
             </Navigation>
-        </Drawer>
-        <Content />
-    </Layout>
-    </div>
-    <div>
-      <Router>
-        <Route exact path="/" component={()=>user? <Member/> : <LoginSignup setUser={setUser}/>}/>
-      </Router>
-      
-    </div>
+          </Drawer>
+          <Content />
+        </Layout>
+      </div>
+      <div>
+        <Router>
+          <Route
+            exact
+            path="/"
+            component={() =>
+              user ? <Member /> : <LoginSignup setUser={setUser} />
+            }
+          />
+        </Router>
+      </div>
     </div>
   );
 }
 // import member from "Member";
 
-
 // Form Function
 function App() {
-  const [developerState, setDeveloperState] = useState({
-   
-  });
+  const [developerState, setDeveloperState] = useState({});
 
   return (
     <div className="card">
-      <div>
-        Name: {developerState.name}
-      </div>
-      <div>
-        Status: {developerState.mood}
-      </div>
-      <div>
-        Lifelong learner: {developerState.lifeLongLearner.toString()}
-      </div>
-      <div>
-        Excitement Level: {developerState.excitementLevel}
-      </div>
+      <div>City Name: {developerState.name}</div>
       <div className="btn-group">
-        <button onClick={() => setDeveloperState({ ...developerState, mood: "lazy" })} className="btn btn-danger">
-          Encourage Laziness
-        </button>
-        <button onClick={() => setDeveloperState({ ...developerState, mood: "determined" })} className="btn btn-success">
-          Fill with Determination
+        <button
+          onClick={() => setDeveloperState({ ...developerState, mood: "lazy" })}
+          className="btn btn-danger"
+        >
+          Search
         </button>
       </div>
     </div>
   );
 }
-
 
 // API Call
 class MyComponent extends React.Component {
@@ -83,10 +82,12 @@ class MyComponent extends React.Component {
   }
 
   componentDidMount() {
-    fetch("https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=35&gsrsearch=")
+    fetch(
+      "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=35&gsrsearch="
+    )
       .then(res => res.json())
       .then(
-        (result) => {
+        result => {
           this.setState({
             isLoaded: true,
             items: result.items
@@ -95,13 +96,13 @@ class MyComponent extends React.Component {
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
-        (error) => {
+        error => {
           this.setState({
             isLoaded: true,
             error
           });
         }
-      )
+      );
   }
 
   render() {
@@ -114,9 +115,7 @@ class MyComponent extends React.Component {
       return (
         <ul>
           {items.map(item => (
-            <li key={item.name}>
-              {item.name} 
-            </li>
+            <li key={item.name}>{item.name}</li>
           ))}
         </ul>
       );
@@ -124,6 +123,4 @@ class MyComponent extends React.Component {
   }
 }
 
-
 export default App;
-
